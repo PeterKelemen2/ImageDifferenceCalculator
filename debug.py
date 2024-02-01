bb722093aa8b1822db3521a9c0d55158b5ee34d7
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-01 09:49:30 +0100

    Added documentation to debug

--- debug.py
+++ debug.py
@@ -1,14 +1,36 @@
+"""
+logger.py - Simple Logger Module
+
+This module provides a basic logging functionality to log messages with timestamps to a text file
+and also print them to the standard error stream. The log file is stored in a 'logs' directory,
+and each session is saved in a separate log file with a timestamp in the filename.
+
+Usage:
+    1. Import the module using: `from logger import log`
+    2. Use the `log` function to log messages.
+
+Example:
+    from logger import log
+
+    log("This is a log message.")
+
+"""
+
 from datetime import datetime
 import sys
 import os
 
+# Global variables for logging
 log_file_path = ""
 log_folder_path = "logs"
-
 session_started = False
 
 
 def init_logger():
+    """
+    Initializes the logger by creating the 'logs' directory and setting up a new log file
+    with a timestamp in the filename.
+    """
     os.makedirs(log_folder_path, exist_ok=True)
 
     global log_file_path
@@ -20,6 +42,15 @@ def init_logger():
 
 
 def log(message):
+    """
+    Logs a message with a timestamp to both a text file and the standard error stream.
+
+    Args:
+        message (str): The message to be logged.
+
+    Note:
+        If a session is not already started, it initializes the logger before logging the message.
+    """
     if session_started:
         date = datetime.now().strftime("[%Y.%m.%d - %H:%M:%S] ")
         with open(log_file_path, 'a') as log_file:
