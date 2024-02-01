873fedb1f9c351ec91d21f617c3e22bd696894bc
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-01 09:20:07 +0100

    Added time to interface

--- interface.py
+++ interface.py
@@ -1,17 +1,34 @@
 import tkinter
+from datetime import datetime
+
 import debug
 
 BGCOLOR = "#00b685"
+WHITE = "#ffffff"
 
 
 class Interface:
     def __init__(self):
         debug.log("Creating interface...")
-        self.root = tkinter.Tk()
-        self.root["bg"] = BGCOLOR
-        self.root.title("Image Difference Calculator")
-        self.root.geometry("500x500")
-        self.root.resizable(False, False)
-        self.root.protocol("WM_DELETE_WINDOW")
+        self.win = tkinter.Tk()
+        self.win["bg"] = BGCOLOR
+        self.win.title("Image Difference Calculator")
+        self.win.geometry("500x500")
+        self.win.resizable(False, False)
+        self.win.protocol("WM_DELETE_WINDOW")
+
+        self.time_label = tkinter.Label(self.win, text=datetime.now().strftime("%Y.%m.%d - %H:%M:%S"))
+        self.time_label.config(font=("Helvetica", 25), fg=WHITE, bg=BGCOLOR)
+
+        self.time_label.place(x=250, y=250, anchor="center")
+
+        self.win.after(1000, self.update_label)
+
         debug.log("Interface created")
-        self.root.mainloop()
+        self.win.mainloop()
+
+    def update_label(self):
+        # Update the label text with the current time
+        self.time_label.config(text=datetime.now().strftime("%Y.%m.%d - %H:%M:%S"))
+        # Schedule the update_label method to be called again after 1000 milliseconds
+        self.win.after(1000, self.update_label)
