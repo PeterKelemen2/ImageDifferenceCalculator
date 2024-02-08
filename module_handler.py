a7e50b16951b08a3d6b53424a7df7c22498d7c73
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-08 11:38:47 +0100

    Improved logging in module_handler.py

--- module_handler.py
+++ module_handler.py
@@ -24,7 +24,7 @@ def module_handler():
     result = subprocess.run(["py", "-m", "pip", "list"], capture_output=True, text=True)
     installed_modules = {line.split()[0] for line in result.stdout.strip().split('\n')[2:]}
 
-    # Install missing modules concurrently
+    # Install missing modules
     missing_modules = [module for module in modules if module not in installed_modules]
     if missing_modules:
         subprocess.run(["py", "-m", "pip", "install", *missing_modules, "--quiet"], check=True, shell=True)
@@ -32,4 +32,4 @@ def module_handler():
     check_if_modules_installed(installed_modules)
     debug.log("All modules installed!")
 
-    print(time.time() - start_time)
+    debug.log("Executed in {:.2f}s".format(time.time() - start_time))
