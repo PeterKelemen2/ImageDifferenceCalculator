f2b297ac3c59a24efb52a996986ac40700dcf7ef
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-08 12:11:20 +0100

    Removed pip upgrade from module_handler.py

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import module_handler
+import vlc_handler
 
-# Call module_handler at the beginning of program
+# Handling dependencies before program starts
 module_handler.module_handler()
+vlc_handler.vlc_installer()
 
 import debug
 import interface
-import vlc_handler
 
 
 def main():
     debug.log("Session started!")
-
     my_interface = interface.Interface()
 
 
 if __name__ == "__main__":
-    # Handling dependencies before program starts
-    # module_handler.module_handler()
-    vlc_handler.vlc_installer()
-
     # Starting program
     main()
--- module_handler.py
+++ module_handler.py
@@ -13,12 +13,28 @@ def check_if_modules_installed(installed_modules):
         sys.exit(f"Module(s) {missing_modules} could not be installed, please install manually")
 
 
+def upgrade_pip():
+    """
+    Upgrade pip if necessary.
+    """
+    try:
+        result = subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], capture_output=True,
+                                text=True)
+        if 'Requirement already up-to-date' in result.stdout:
+            debug.log("pip is already up-to-date.")
+        else:
+            debug.log("pip upgraded successfully.")
+    except subprocess.CalledProcessError as e:
+        debug.log(f"Error upgrading pip: {e.stderr}")
+        sys.exit("Error upgrading pip.")
+
+
 def module_handler():
     start_time = time.time()
 
-    # Upgrade pip to ensure it's up-to-date
-    debug.log("Upgrading pip if necessary...")
-    subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], check=True, shell=True)
+    # # Upgrade pip to ensure it's up-to-date
+    # debug.log("Upgrading pip if necessary...")
+    # subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], check=True, shell=True)
 
     # Get installed modules
     result = subprocess.run(["py", "-m", "pip", "list"], capture_output=True, text=True)
