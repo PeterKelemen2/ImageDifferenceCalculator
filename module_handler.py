c6aa35cfbe4314598d75d25554cef78b031595a7
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-08 14:17:43 +0100

    Made module_handler.py more readable

--- module_handler.py
+++ module_handler.py
@@ -8,7 +8,9 @@ modules = ["opencv-python", "pillow"]
 
 
 def check_if_modules_installed(installed_modules):
-    missing_modules = [module for module in modules if module not in installed_modules]
+    missing_modules = [module
+                       for module in modules
+                       if module not in installed_modules]
     if missing_modules:
         debug.log(f"Module(s) {missing_modules} could not be installed, please install manually")
         sys.exit(f"Module(s) {missing_modules} could not be installed, please install manually")
@@ -19,7 +21,8 @@ def upgrade_pip():
     Upgrade pip if necessary.
     """
     try:
-        result = subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], capture_output=True,
+        result = subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"],
+                                capture_output=True,
                                 text=True)
         if 'Requirement already up-to-date' in result.stdout:
             debug.log("pip is already up-to-date.")
@@ -38,13 +41,18 @@ def module_handler():
     # subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], check=True, shell=True)
 
     # Get installed modules
-    result = subprocess.run(["py", "-m", "pip", "list"], capture_output=True, text=True)
-    installed_modules = {line.split()[0] for line in result.stdout.strip().split('\n')[2:]}
+    result = subprocess.run(["py", "-m", "pip", "list"],
+                            capture_output=True,
+                            text=True)
+    installed_modules = {line.split()[0]
+                         for line in result.stdout.strip().split('\n')[2:]}
 
     # Install missing modules
     missing_modules = [module for module in modules if module not in installed_modules]
     if missing_modules:
-        subprocess.run(["py", "-m", "pip", "install", *missing_modules, "--quiet"], check=True, shell=True)
+        subprocess.run(["py", "-m", "pip", "install", *missing_modules, "--quiet"],
+                       check=True,
+                       shell=True)
 
     check_if_modules_installed(installed_modules)
     debug.log("All modules installed!")
