a328983c79e01ad48f8fd204bcca2ae4baf3563b
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-08 10:05:56 +0100

    Improved module installer speed

--- module_handler.py
+++ module_handler.py
@@ -1,5 +1,6 @@
 import importlib.util
 import subprocess
+import time
 
 modules = ["numpy", "opencv-python", "pillow", "python-vlc"]
 
@@ -9,12 +10,13 @@ def install_module(module: str):
 
 
 def module_handler():
-    subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip"], shell=True)
-    for module in modules:
-        if importlib.util.find_spec(module) is None:
-            install_module(module)
+    subprocess.run(["py", "-m", "pip", "install", "--upgrade", "pip", "--quiet"], shell=True)
+    start_time = time.time()
 
-        # try:
-        #     import module
-        # except ImportError:
-        #     subprocess.run(["py", "-m", "pip", "install", module], shell=True)
+    modules_to_install = [module for module in modules if importlib.util.find_spec(module) is None]
+    if modules_to_install:
+        subprocess.run(["py", "-m", "pip", "install", *modules_to_install], shell=True)
+
+    end_time = time.time()
+    elapsed_time = "{:.2f}".format(end_time - start_time)
+    print(f"Elapsed time: {elapsed_time}s")
