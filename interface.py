45915051449b4bccf7f914405f4271034010b3cf
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-09 08:37:23 +0100

    Adjusted coloring of debug messages

--- interface.py
+++ interface.py
@@ -29,18 +29,18 @@ class Interface:
         self.time_label = None
         self.time_wrapper = None
 
-        debug.log("[1/1] Creating interface...")
+        debug.log("[1/1] Creating interface...", text_color="blue")
 
         self.set_properties()
         self.create_time_frame()
         self.create_browser()
 
-        debug.log("[1/2] Interface created")
+        debug.log("[1/2] Interface created", text_color="blue")
         self.win.mainloop()
 
     def set_properties(self):
         # Set windows properties
-        debug.log("[2/1] Setting properties...")
+        debug.log("[2/1] Setting properties...", text_color="magenta")
 
         self.win = Tk()
         self.win["bg"] = BGCOLOR
@@ -50,7 +50,7 @@ class Interface:
         self.win.protocol("WM_DELETE_WINDOW")
         self.selected_file_path = StringVar()
 
-        debug.log("[2/2] Properties set!")
+        debug.log("[2/2] Properties set!", text_color="magenta")
 
     def update_label(self):
         # Update the label text with the current time
@@ -60,7 +60,7 @@ class Interface:
 
     def create_time_frame(self):
         # Wrapper for time Label
-        debug.log("[3/1] Creating Time Frame wrapper...")
+        debug.log("[3/1] Creating Time Frame wrapper...", text_color="yellow")
 
         self.time_wrapper = LabelFrame(self.win,
                                        text="Time",
@@ -70,39 +70,39 @@ class Interface:
 
         self.time_wrapper.place(x=10, y=5)
 
-        debug.log("[3/2] Time wrapper created!")
+        debug.log("[3/2] Time wrapper created!", text_color="yellow")
 
         # Label that shows the current time and date
-        debug.log("[3/3] Creating Time Label...")
+        debug.log("[3/3] Creating Time Label...", text_color="yellow")
 
         self.time_label = Label(self.time_wrapper, text=datetime.now().strftime("%Y.%m.%d - %H:%M:%S"))
         self.time_label.config(font=("Helvetica", TIME_FONT_SIZE),  # Font size
                                fg=BLACK,  # Font color
                                bg=BGCOLOR)  # Background color
 
-        debug.log("[3/4] Time Label created!")
+        debug.log("[3/4] Time Label created!", text_color="yellow")
 
         # Set Frame label to width and height of Label
-        debug.log("[3/5] Updating Time Label config...")
+        debug.log("[3/5] Updating Time Label config...", text_color="yellow")
         label_width = self.time_label.winfo_reqwidth() + 20
         label_height = self.time_label.winfo_reqheight() * 2
         self.time_wrapper.config(font=("Helvetica", TIME_FONT_SIZE, "bold"),
                                  width=label_width,
                                  height=label_height)
-        debug.log("[3/6] Time Label config updated!")
+        debug.log("[3/6] Time Label config updated!", text_color="yellow")
 
         # Pack Label in Frame
-        debug.log("[3/7] Packing Time Label in Frame...")
+        debug.log("[3/7] Packing Time Label in Frame...", text_color="yellow")
         self.time_label.pack(padx=10,
                              pady=5,
                              anchor="center")
-        debug.log("[3/8] Time Label packed!")
+        debug.log("[3/8] Time Label packed!", text_color="yellow")
         # Schedule the update_label method to be called
         self.win.after(1000, self.update_label)
 
     def create_browser(self):
         # Wrapper for file browsing
-        debug.log("[4/1] Creating Browsing wrapper...")
+        debug.log("[4/1] Creating Browsing wrapper...", text_color="magenta")
         button_wrapper = LabelFrame(self.win,
                                     text="Input file",
                                     bg=BGCOLOR,
@@ -112,10 +112,10 @@ class Interface:
         # Place to the right
         x_coordinate = WIN_WIDTH - button_wrapper.winfo_reqwidth() - 10
         button_wrapper.place(x=x_coordinate, y=5)
-        debug.log("[4/2] Browsing wrapper created!")
+        debug.log("[4/2] Browsing wrapper created!", text_color="magenta")
 
         # Button to initiate browsing
-        debug.log("[4/3] Creating browse Button...")
+        debug.log("[4/3] Creating browse Button...", text_color="magenta")
 
         browse_button = custom_button.RoundedRectangleButton(button_wrapper,
                                                              text="Browse",
@@ -124,10 +124,10 @@ class Interface:
                                                              height=30)
         browse_button.canvas.place(x=10, y=10)
 
-        debug.log("[4/4] Browse Button created!")
+        debug.log("[4/4] Browse Button created!", text_color="magenta")
 
         # Label for showing opened file path
-        debug.log("[4/5] Creating file path Label...")
+        debug.log("[4/5] Creating file path Label...", text_color="magenta")
         opened_file_label = Label(button_wrapper,
                                   textvariable=self.selected_file_path,
                                   bg=BGCOLOR,
@@ -137,11 +137,11 @@ class Interface:
         # Place right to the button, vertically centered
         opened_file_label.place(x=browse_button.winfo_reqwidth() * 2 - 55,
                                 y=browse_button.winfo_reqheight() / 2 - 5)
-        debug.log("[4/6] File path Label created!")
+        debug.log("[4/6] File path Label created!", text_color="magenta")
 
     def browse_files(self):
         # Open a file dialog and get the selected file path
-        debug.log("Opening file browser dialog...")
+        debug.log("Opening file browser dialog...", text_color="magenta")
         file_path = filedialog.askopenfilename(title="Select a file",
                                                filetypes=[("Video Files", "*.mp4;*.avi;*.mkv;*.mov;*.wmv"),
                                                           ("Image Files", "*.png;*.jpg;*.jpeg;*.gif"),
@@ -150,11 +150,11 @@ class Interface:
 
         # Update the label with the selected file path
         self.selected_file_path.set(file_path)
-        debug.log(f"Selected file: {file_path}")
+        debug.log(f"Selected file: {file_path}", text_color="blue")
         if file_path:
             self.show_first_frame_details(file_path)
         else:
-            debug.log("No file selected")
+            debug.log("No file selected", text_color="red")
 
     def show_first_frame_details(self, path: str):
         """
@@ -168,7 +168,7 @@ class Interface:
         """
 
         # Creating a labeled frame to contain video details
-        debug.log("[5/1] Creating video details wrapper...")
+        debug.log("[5/1] Creating video details wrapper...", text_color="yellow")
         frame_wrapper = LabelFrame(self.win,
                                    text="Video Data",
                                    bg=BGCOLOR,
@@ -176,22 +176,22 @@ class Interface:
                                    height=320,
                                    font=("Helvetica", 10, "bold"))
         frame_wrapper.place(x=10, y=100)
-        debug.log("[5/2] Video details wrapper created!")
+        debug.log("[5/2] Video details wrapper created!", text_color="yellow")
 
         # Creating a label to display the first frame of the video
-        debug.log("[5/3] Creating placeholder label for first frame...")
+        debug.log("[5/3] Creating placeholder label for first frame...", text_color="yellow")
         frame_label = Label(frame_wrapper)
         frame_label.place(x=5, y=5)
-        debug.log("[5/4] First frame placeholder created!")
+        debug.log("[5/4] First frame placeholder created!", text_color="yellow")
 
         # Opening the video and extracting details from the first frame
-        debug.log("[5/5] Opening video file and getting first frame data")
+        debug.log("[5/5] Opening video file and getting first frame data...", text_color="yellow")
         cap = cv2.VideoCapture(path)
         fps = "{:.2f}".format(cap.get(cv2.CAP_PROP_FPS))
         bitrate = "{:.0f}".format(cap.get(cv2.CAP_PROP_BITRATE))
         ret, frame = cap.read()
         cap.release()
-        debug.log("[5/6] First frame data gathered!")
+        debug.log("[5/6] First frame data gathered!", text_color="yellow")
 
         if ret:
             frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -206,18 +206,18 @@ class Interface:
                       f"Bitrate: {bitrate} kbps")
 
             # Resizing the first frame to fit within the frame wrapper
-            debug.log("[5/7] Calculating first frame information...")
+            debug.log("[5/7] Calculating first frame information...", text_color="yellow")
             new_width = frame_wrapper.winfo_reqwidth() // 2
             new_height = int(new_width / aspect_ratio)
             image = image.resize((new_width, new_height), Image.BILINEAR)
             image_file = ImageTk.PhotoImage(image)
-            debug.log("[5/8] First image information set!")
+            debug.log("[5/8] First image information set!", text_color="yellow")
 
             # Configuring the frame label with the resized first frame
-            debug.log("[5/9] Configuring image...")
+            debug.log("[5/9] Configuring image...", text_color="yellow")
             frame_label.config(image=image_file)
             frame_label.image = image_file
-            debug.log("[5/10] Image configured!")
+            debug.log("[5/10] Image configured!", text_color="yellow")
 
         # media_player_button = Button(frame_wrapper, text="Open Media Player")
         media_player_button = custom_button.RoundedRectangleButton(frame_wrapper,
@@ -228,7 +228,7 @@ class Interface:
                                          y=new_height + 15)
 
         # Creating labels to display video details
-        debug.log("[5/11] Creating labels to display video details...")
+        debug.log("[5/11] Creating labels to display video details...", text_color="yellow")
         frame_details_header = Label(frame_wrapper,
                                      text="Video details:",
                                      bg=BGCOLOR,
@@ -244,7 +244,7 @@ class Interface:
                               anchor="w"
                               )
         image_details.place(x=new_width + 30, y=frame_details_header.winfo_reqheight() * 1.5)
-        debug.log("[5/12] Labels to display video details created!")
+        debug.log("[5/12] Labels to display video details created!", text_color="yellow")
 
         # self.create_progress_bar(frame_wrapper)
 
--- vlc_handler.py
+++ vlc_handler.py
@@ -2,7 +2,6 @@ import os.path
 import platform
 import subprocess
 import sys
-import time
 
 import debug
 
@@ -17,12 +16,12 @@ tries: int = 0
 def vlc_windows_installer():
     global tries
     tries += 1
-    debug.log(f"Windows version: {platform.version()}")
+    debug.log(f"Windows version: {platform.version()}", text_color="cyan")
     # Checking if VLC path exists
     if os.path.exists(windows_vlc_path):
         debug.log("VLC installed!", text_color="cyan")
     else:
-        debug.log("VLC not installed, installing...")
+        debug.log("VLC not installed, installing...", text_color="yellow")
         # Download VLC installer
         subprocess.run(["curl", "-LO", "https://get.videolan.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe"])
         # Run the installer
@@ -30,12 +29,13 @@ def vlc_windows_installer():
 
         # Checking if install was successful
         if os.path.exists(windows_vlc_path):
-            debug.log("VLC installed successfully!")
+            debug.log("VLC installed successfully!", text_color="blue")
         else:
             # If unsuccessful, try again (only 3 times)
             if tries < 3:
                 vlc_installer()
             else:
+                debug.log("Could not install VLC, install manually to default location, exiting...", text_color="red")
                 sys.exit("Could not install VLC, install manually to default location, exiting...")
 
 
@@ -56,12 +56,12 @@ def vlc_installer():
     elif current_os == "Linux":
         vlc_linux_installer()
     else:
-        debug.log("OS not supported!")
+        debug.log("OS not supported!", text_color="red")
         sys.exit("OS not supported")
 
 
 def open_video(file_path: str):
-    debug.log("Opening video...")
+    debug.log("Opening video in VLC...", text_color="blue")
     if current_os == "Windows":
         file_path = file_path.replace("/", "\\")
         subprocess.Popen([windows_vlc_path, "--loop", file_path])
