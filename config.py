542a10460f0972e3f78d0e7ea229a17b6e2a120b
Kelemen Péter <peter.kelemen456@gmail.com>
2024-02-20 14:29:43 +0100

    Added documentation to config.py

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 import os
-
 import debug
 
 config_path = "settings.config"
 
 
 def init_settings():
+    """
+    Initializes settings by creating a configuration file with default values if it doesn't exist.
+
+    This function checks if the configuration file specified by `config_path` exists. If not,
+    it creates the file and writes default settings to it (language as 'english' and theme as 'palenight').
+
+    Global Variables:
+        config_path (str): Path to the configuration file.
+    """
     global config_path
     if not os.path.exists(config_path):
         try:
@@ -16,7 +24,14 @@ def init_settings():
             debug.log(str(e), text_color="red")
 
 
-def save_settings(settings):
+def save_settings(settings: list):
+    """
+    Saves settings to the configuration file.
+    This function writes the provided settings to the configuration file specified by `config_path`.
+
+    Parameters:
+        settings (list): A list containing language and theme settings.
+    """
     with open(config_path, 'w', encoding="utf-8") as file:
         file.write("lang=" + settings[0] + "\n")
         file.write("theme=" + settings[1] + "\n")
@@ -24,6 +39,14 @@ def save_settings(settings):
 
 
 def load_settings():
+    """
+    Loads settings from the configuration file.
+    This function reads settings from the configuration file specified by `config_path`,
+    extracts language and theme settings, and returns them as a list.
+
+    Returns:
+        list: A list containing language and theme settings.
+    """
     global config_path
     lines = list()
     with open(config_path, 'r', encoding="utf-8") as file:
