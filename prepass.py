583ecd0493a47360e6dcb8eba9d7a9c6301a807f
Kelemen Péter <peter.kelemen456@gmail.com>
2024-03-18 13:10:25 +0100

    Added option to plot to prepass method

--- prepass.py
+++ prepass.py
@@ -1,3 +1,5 @@
+import time
+
 import debug
 import cv2
 import numpy as np
@@ -5,12 +7,30 @@ import matplotlib.pyplot as plt
 
 
 def calculate_avg_brightness(frame):
-    return frame.sum() // frame.size
+    # return frame.sum() // frame.size
+    return np.mean(frame)
 
 
-def plot_average_brightness(before_list, after_list, title="Average", path=None):
-    x_values = range(len(before_list))
-    plt.figure(figsize=(10, 4))
+def print_as_table_row(i, curr, first, delta):
+    # print(
+    #     f"[{i}] {curr} (Difference: {curr - first} | {delta})")
+
+    index_space = 4 - len(str(i))
+    curr_space = 22 - len(str(curr))
+    diff_space = 22 - len(str(curr - first))
+    delta_space = 6 - len(str(delta))
+
+    line = (" " * index_space + str(i) +
+            " | " + " " * curr_space + str(curr) +
+            " | " + " " * diff_space + str(curr - first) +
+            " | " + " " * delta_space + str(delta))
+    print(line)
+
+
+def plot_average_brightness(before_list, after_list, title="Graph", path=None):
+    x_values = np.arange(len(before_list))
+    # x_values = range(len(before_list))
+    plt.figure(figsize=(8, 4))
     plt.plot(x_values, before_list, linestyle='-', color='red', label="Before")
     plt.plot(x_values, after_list, linestyle='-', color='blue', label="After")
     plt.xlabel("Index")
@@ -18,16 +38,19 @@ def plot_average_brightness(before_list, after_list, title="Average", path=None)
     plt.title(title)
     plt.grid(True)
     plt.ylim(min(before_list) - 1, max(before_list) + 1)
+    plt.xlim(0, len(x_values) - 1)
     plt.legend()
     if path:
         plt.savefig(path[:-4] + "_prepass_plot.png", dpi=150)
     plt.show()
 
 
-def preprocess(path):
+def preprocess(path, to_plot=True):
+    start_time = time.time()
+
     cap = cv2.VideoCapture(path)
 
-    new_path = path[:-4] + '_pass.mp4'
+    new_path = path[:-4] + '_prepass.mp4'
     ret, first_frame = cap.read()
     frame_height, frame_width = first_frame.shape[:2]
 
@@ -35,43 +58,36 @@ def preprocess(path):
     output = cv2.VideoWriter(new_path, codec, 95, (frame_width, frame_height))
 
     curr_index = 0
-    prev_frame = first_frame
+    first_frame_brightness = calculate_avg_brightness(first_frame)
 
-    b_list = list()
-    prepass_b_list = list()
+    b_list, prepass_b_list = [], []
 
     while True:
         ret, frame = cap.read()
         if not ret:
             break
-
         curr_index += 1
 
         curr_brightness = calculate_avg_brightness(frame)
-        prev_brightness = calculate_avg_brightness(prev_frame)
-
         b_list.append(curr_brightness)
 
-        delta_brightness = 1 - ((curr_brightness - prev_brightness) / curr_brightness)
-        if curr_brightness - prev_brightness == 0:  # or delta_brightness > 1.005:
+        delta_brightness = 1 - ((curr_brightness - first_frame_brightness) / curr_brightness)
+        # delta_brightness = round(1 - ((curr_brightness - first_frame_brightness) / curr_brightness), 4)
+        if curr_brightness - first_frame_brightness == 0:
             delta_brightness = 1
 
-        print(
-            f"[Frame: {curr_index}] {curr_brightness} (Difference: {curr_brightness - prev_brightness} | {delta_brightness})")
-        frame = cv2.convertScaleAbs(frame, alpha=delta_brightness, beta=0)
+        print_as_table_row(curr_index, curr_brightness, first_frame_brightness, delta_brightness)
 
+        frame = cv2.convertScaleAbs(frame, alpha=delta_brightness, beta=0)
         prepass_b_list.append(calculate_avg_brightness(frame))
 
         output.write(frame)
 
-        # prev_frame = frame
-
     cap.release()
     output.release()
+    if to_plot:
+        plot_average_brightness(before_list=b_list, after_list=prepass_b_list, title="Brightness regulation", path=path)
+    debug.log(f"Prepass finished in {"{:.2f}s".format(time.time() - start_time)}", text_color="cyan")
 
-    plot_average_brightness(before_list=b_list, after_list=prepass_b_list, title="Before regulation", path=path)
 
-
-preprocess('C:/sample_newly_stabilized.mp4')
-# preprocess('C:/sample_newly_stabilized_pass.mp4')
-# preprocess('C:/sample_newly_stabilized_pass_pass.mp4')
+preprocess('C:/sample_newly_stabilized.mp4', to_plot=True)
