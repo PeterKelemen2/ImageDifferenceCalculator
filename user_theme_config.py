973917d37e204bdf4ebe3b8677e94f8101db2229
Kelemen Péter <peter.kelemen456@gmail.com>
2024-04-22 14:39:49 +0200

    Handling user theme file creation

--- user_theme_config.py
+++ user_theme_config.py
@@ -1,4 +1,12 @@
 import json
+import os
+
+theme_path = "user_theme.json"
+default_user_theme = '''{
+    "bg": "#97F3FF",
+    "accent": "#FFF3A4",
+    "text": "#000000"
+}'''
 
 
 class UserTheme:
@@ -14,18 +22,23 @@ class UserTheme:
 def save_theme(theme):
     try:
         # theme_dict = theme.to_dict()
-        with open("user_theme.json", "w") as json_file:
+        with open(theme_path, "w") as json_file:
             json.dump(theme, json_file, indent=4)
     except FileNotFoundError:
         print("Error.")
 
 
 def load_theme():
-    try:
-        with open("user_theme.json", "r") as json_file:
-            global user_theme
+    if os.path.exists(theme_path):
+        try:
+            with open(theme_path, "r") as json_file:
+                user_theme = json.load(json_file)
+            return user_theme
+        except FileNotFoundError:
+            print("Error.")
+    else:
+        with open(theme_path, "w") as json_file:
+            json_file.write(default_user_theme)
+        with open(theme_path, "r") as json_file:
             user_theme = json.load(json_file)
-        print(user_theme)
         return user_theme
-    except FileNotFoundError:
-        print("Error.")
